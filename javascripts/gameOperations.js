3c4d052f822070f1d729f6dc86d83b12ea7b0f92
Jaradus <ethanschmertzler@mac.com>
2014-03-24 22:46:36 -0400

    Silos can be added to the DOM loaded with missiles

--- javascripts/gameOperations.js
+++ javascripts/gameOperations.js
@@ -1,3 +1,39 @@
-var GameOperations = {
-  
+var gameOperations = {
+
+  lastMouseLatLong: null,
+ 
+  setMouseLatLongWatcher: function(){
+    // Returns the longitude and latitude of where the mouse is clicked
+    // e.g., [-84.35368421052631, 20.691987822319366] (long, lat)
+    d3.select("#draggable").on("mouseup.log",function(){
+                                                          gameOperations.lastMouseLatLong = {
+                                                                      longitude: map.projection.invert(d3.mouse(this))[0],
+                                                                      latitude: map.projection.invert(d3.mouse(this))[1]
+                                                                      };
+                                                          })
+  },
+
+
+  deploySilos: function() {
+        $(".silo").draggable();
+
+        $("#container1").droppable({
+            accept: "#draggable",
+            drop: function( event, ui ) {
+
+                                        console.log(gameOperations.lastMouseLatLong) 
+                                        }
+        });
+  }
+
+
+
 }
+
+
+$(function(){
+
+  gameOperations.setMouseLatLongWatcher();
+  gameOperations.deploySilos();
+
+})
--- javascripts/silos.js
+++ javascripts/silos.js
@@ -4,21 +4,27 @@ var Silo = Backbone.Model.extend({
     missiles: [],
     location: {
       longitude: null,
-      lattitude: null
+      latitude: null
     }
   },
+
   initialize: function(){
-    self = this
+    var self = this;
+
+    _.times(5, function(){
+      var newMissile = new Missile({
+        silo: self
+      });
+
+      self.attributes.missiles.push(newMissile);
+
+    });
+
     self.view = new SiloView({
       model: self
     });
-    _.times(4, function(){
-      newMissile = new Missile({
-        silo: self
-      });
-      self.attributes.missiles.push(newMissile);
-    });
   }
+
 });
 
 var SiloCollection = Backbone.Collection.extend({
@@ -26,12 +32,25 @@ var SiloCollection = Backbone.Collection.extend({
 })
 
 var SiloView = Backbone.View.extend({
-  // tagName: null,
-  // template: null,
-  // initialize: function(){
+  tagName: 'div',
+  className: 'silo',
+  el: '#silo_container',
 
-  // },
-  // render: function(){
+  render: function(){
+    var self = this;
+    this.$el.html(this.template(this.model.attributes))
+    return this
+  },
 
-  // }
+  initialize: function(){
+    var self = this;
+    this.render();
+  },
+
+  template: _.template("<svg xmlns='http://www.w3.org/2000/svg' version='1.1'><polygon style='fill:none;stroke:green;stroke-width:2.5px' points='3,18 15,2 27,18'> </polygon> <text style='text-align:center;' x='0' y='30' font-size='10'><%= this.model.attributes.missiles.length  %></text></svg>"),
+
+})
+
+var SiloListView = Backbone.View.extend({
+
 })
